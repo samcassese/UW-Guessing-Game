90e28727e554058c815aa9e121085e5153e99f31
Sam Cassese <92000097+samcassese@users.noreply.github.com>
2022-12-07 17:31:48 +0000

    Started making it object oriented

--- GG.java
+++ GG.java
@@ -1,19 +1,117 @@
-// this class is for overall results at the end i think
+/*
+Sam Cassese
+12/7/22
+Period 2
+*/
+
+/*
+learning the difference between object-oriented and procedueral code
+we are going to let the GG remember our past guesses
+*/
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class GG {
-  
+
   // Creat properties for each value in the overal results
   // from the Guessing Game
+  //////////////////////
+  // PROPERTIES
+  //////////////////////
   private String player;
-  
-  public GG(String player){
+  private int totalGuesses;
+  private int totalGames;
+  private int bestGame;
+  private int goal;
+  private double guessesPerGame;
+
+  //////////////////////
+  // CONSTRUCTORS
+  /////////////////////
+  public GG(String player) throws FileNotFoundException {
+
+    File f = new File("ledger.tsv");
+    Scanner scanner = new Scanner(f);
+
+    while(scanner.hasNextLine()) {
+      
+      if(player.equals(scanner.next())) {
+        
+        this.totalGames = scanner.nextInt();
+        this.totalGuesses = scanner.nextInt();
+        this.bestGame = scanner.nextInt();
+
+        System.out.println("-------------");
+        
+      }
+      
+    }
     
+
     this.player = player;
     welcome();
-    
+
+
   }
 
-  private void welcome(){
-    System.out.println("WELCOME: " + this.player);
+  ///////////////////////
+  // METHODS
+  //////////////////////
+
+  public void playGame() {
+    this.totalGames++;
+    this.totalGuesses+= 6;
+    this.bestGame = 6;
+  }
+
+  public void showResults() {
+
+    guessesPerGame = totalGuesses / totalGames;
+
+    System.out.println("\n" + "Results for " + this.player + ":");
+    System.out.printf("Total games = %d \n", this.totalGames);
+    System.out.printf("Total guesses = %d \n", this.totalGuesses);
+    System.out.printf("Guesses/game = %f \n", this.guessesPerGame);
+    System.out.printf("Best game = %d", this.bestGame);
+    
   }
   
+  public String getPlayer() {
+    return player;
+  }
+
+  public void setPlayer(String player) {
+    this.player = player;
+  }
+
+  public int getTotalGuesses() {
+    return totalGuesses;
+  }
+
+  public void setTotalGuesses(int totalGuesses) {
+    this.totalGuesses = totalGuesses;
+  }
+
+  public int getTotalGames() {
+    return totalGames;
+  }
+
+  public void setTotalGames(int totalGames) {
+    this.totalGames = totalGames;
+  }
+
+  public int getGoal() {
+    return goal;
+  }
+
+  public void setGoal(int goal) {
+    this.goal = goal;
+  }
+
+  private void welcome() {
+    System.out.println("WELCOME: " + this.player);
+  }
+
 }
--- Main.java
+++ Main.java
@@ -5,12 +5,22 @@ AP CS-A
 Period 2
 */
 
+import java.io.FileNotFoundException;
+
 class Main {
   
-  public static void main(String[] args) {
+  public static void main(String[] args) throws FileNotFoundException {
     
     String a[] = {"31","64","49"};
-    GussingGame.main(a);
+    //GussingGame.main(a);
+
+    GG myGuessingGame = new GG("Sam");
+    GG gg = new GG("Sue");
+    System.out.println(myGuessingGame.getPlayer());
+    System.out.println(gg.getPlayer());
+
+    gg.playGame();
+    gg.showResults();
 
     // Can you make a Guessing Game Type that could be used to get 
     // each value from the overall results via accessor methods?
